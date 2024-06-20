20854b16b7378574455396924b04fc8b19c2b881
Ben Miles <bcgm3studios@gmail.com>
2024-06-19 23:51:15 -0400

    Set Up Basic Horizontal Movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,20 @@
 extends CharacterBody2D
 
+@export var gravity = 400
+@export var speed = 125
+
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _process(delta):
-	if(Input.is_action_just_pressed("player_move_right")):
-		animated_sprite.play("run")
+func _physics_process(delta):
+	if(!is_on_floor()):
+		velocity.y += gravity * delta
+	
+	var direction = Input.get_axis("player_move_left", "player_move_right")
+	velocity.x = direction * speed
+	
+#	if(velocity.x != 0):
+#		animated_sprite.play("run")
+#	else:
+#		animated_sprite.play("idle")
+		
+	move_and_slide()
