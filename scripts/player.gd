e56bcde511fdbcdc8a4a04804897f51c3bcad62d
Ben Miles <bcgm3studios@gmail.com>
2024-06-19 23:36:23 -0400

    Player Input / Animation Test

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,2 +1,7 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _process(delta):
+	if(Input.is_action_just_pressed("player_move_right")):
+		animated_sprite.play("run")
