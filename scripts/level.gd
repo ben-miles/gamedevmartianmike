af01944a74b2488777861a9178f06e01ebad7a6b
Ben Miles <bcgm3studios@gmail.com>
2024-07-07 00:14:33 -0400

    Make Deathzone Node into its own Scene
    This becomes necessary so we can instance it into additional levels.

--- scripts/level.gd
+++ scripts/level.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 @onready var start = $Start
 @onready var exit = $Exit
+@onready var deathzone = $Deathzone
 
 var player = null
 
@@ -16,6 +17,7 @@ func _ready():
 		trap.connect("touched_player", _on_trap_touched_player)
 #		trap.touched_player.connect(_on_trap_touched_player)
 	exit.body_entered.connect(_on_exit_body_entered)
+	deathzone.body_entered.connect(_on_deathzone_body_entered)
 
 func _process(delta):
 	if(Input.is_action_just_pressed("quit")):
